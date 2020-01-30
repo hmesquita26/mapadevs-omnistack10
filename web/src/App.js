3c1c746d497f031f19592a4e145601371652976a
Heytor Mesquita <heytormesquita@gmail.com>
2020-01-29 22:59:49 -0300

    adiciona estilizaçao da main e pega dados de localizaçao

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css';
 
 function App() {
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const {latitude, longitude} = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    );
+  }, [])
+
+
   return (
     <div id="app">
       <aside>
@@ -28,19 +51,82 @@ function App() {
           <div className="input-group">
             <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required/>
+              <input 
+                type="number" 
+                name="latitude" 
+                id="latitude" 
+                required 
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}/>
             </div>
 
             <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required/>
+              <input 
+                type="number" 
+                name="longitude" 
+                id="longitude" 
+                required 
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}  
+              />
             </div>  
           </div>
           <button type="submit">Salvar</button>
         </form>
       </aside>
-      <main>
 
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/20123157?s=460&v=4" alt="Heytor Mesquita"/>
+              <div className="user-info">
+                <strong>Heytor Mesquita</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Estudante de Tecnologia da Informação no IMD/UFRN</p>
+            <a href="https://github.com/hmesquita26">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/20123157?s=460&v=4" alt="Heytor Mesquita"/>
+              <div className="user-info">
+                <strong>Heytor Mesquita</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Estudante de Tecnologia da Informação no IMD/UFRN</p>
+            <a href="https://github.com/hmesquita26">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/20123157?s=460&v=4" alt="Heytor Mesquita"/>
+              <div className="user-info">
+                <strong>Heytor Mesquita</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Estudante de Tecnologia da Informação no IMD/UFRN</p>
+            <a href="https://github.com/hmesquita26">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/20123157?s=460&v=4" alt="Heytor Mesquita"/>
+              <div className="user-info">
+                <strong>Heytor Mesquita</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Estudante de Tecnologia da Informação no IMD/UFRN</p>
+            <a href="https://github.com/hmesquita26">Acessar perfil no Github</a>
+          </li>
+
+        </ul>
       </main>
     </div>
   );
