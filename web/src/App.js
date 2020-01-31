ef860915db9688a29bcb3c2503eb279b3b4c3ee0
Heytor Mesquita <heytormesquita@gmail.com>
2020-01-31 00:18:24 -0300

    adiciona serapaçao de componentes e finaliza estilizaçao do layout

--- web/src/App.js
+++ web/src/App.js
@@ -1,131 +1,44 @@
 import React, {useState, useEffect} from 'react';
-
+import api from './services/api';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
-function App() {
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
 
+function App() {
+  const [devs, setDevs] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const {latitude, longitude} = position.coords;
+    async function loadDevs() {
+      const response = await api.get('/devs');
 
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (err) => {
-        console.log(err);
-      },
-      {
-        timeout: 30000,
-      }
-    );
+      setDevs(response.data);
+    }
+
+    loadDevs();
   }, [])
 
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="github_url">Link do Github</label>
-            <input name="github_url" id="github_url" required/>
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input 
-                type="number" 
-                name="latitude" 
-                id="latitude" 
-                required 
-                value={latitude}
-                onChange={e => setLatitude(e.target.value)}/>
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input 
-                type="number" 
-                name="longitude" 
-                id="longitude" 
-                required 
-                value={longitude}
-                onChange={e => setLongitude(e.target.value)}  
-              />
-            </div>  
-          </div>
-          <button type="submit">Salvar</button>
-        </form>
+          <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/20123157?s=460&v=4" alt="Heytor Mesquita"/>
-              <div className="user-info">
-                <strong>Heytor Mesquita</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Estudante de Tecnologia da Informação no IMD/UFRN</p>
-            <a href="https://github.com/hmesquita26">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/20123157?s=460&v=4" alt="Heytor Mesquita"/>
-              <div className="user-info">
-                <strong>Heytor Mesquita</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Estudante de Tecnologia da Informação no IMD/UFRN</p>
-            <a href="https://github.com/hmesquita26">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/20123157?s=460&v=4" alt="Heytor Mesquita"/>
-              <div className="user-info">
-                <strong>Heytor Mesquita</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Estudante de Tecnologia da Informação no IMD/UFRN</p>
-            <a href="https://github.com/hmesquita26">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/20123157?s=460&v=4" alt="Heytor Mesquita"/>
-              <div className="user-info">
-                <strong>Heytor Mesquita</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Estudante de Tecnologia da Informação no IMD/UFRN</p>
-            <a href="https://github.com/hmesquita26">Acessar perfil no Github</a>
-          </li>
-
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
         </ul>
       </main>
     </div>
