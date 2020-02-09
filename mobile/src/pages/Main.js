e34423056837cb5e0e8b12659180f0279f568830
Heytor Mesquita <heytormesquita@gmail.com>
2020-02-09 00:10:46 -0300

    adiciona form e button para buscar devs

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync , getCurrentPositionAsync} from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }){
   const [currentRegion, setCurrentRegion] = useState(null);
@@ -34,20 +35,34 @@ function Main({ navigation }){
   }
   
   return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{latitude: -5.8583156, longitude: -35.3418353}}>
-        <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/20123157?s=460&v=4' }} />
-        <Callout onPress={()=> {
-          navigation.navigate('Profile', { github_username: 'hmesquita26' });
-        }}>
-          <View style={styles.callout}>
-            <Text style={styles.devName}>Heytor Mesquita</Text>
-            <Text style={styles.devBio}>Estudante de Tecnologia da Informação na UFRN.</Text>
-            <Text style={styles.devTechs}>ReactJS, React Native, Node.js</Text>
-          </View>
-        </Callout>
-      </Marker>
-    </MapView>
+    <>
+      <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{latitude: -5.8583156, longitude: -35.3418353}}>
+          <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/20123157?s=460&v=4' }} />
+          <Callout onPress={()=> {
+            navigation.navigate('Profile', { github_username: 'hmesquita26' });
+          }}>
+            <View style={styles.callout}>
+              <Text style={styles.devName}>Heytor Mesquita</Text>
+              <Text style={styles.devBio}>Estudante de Tecnologia da Informação na UFRN.</Text>
+              <Text style={styles.devTechs}>ReactJS, React Native, Node.js</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+      <View style={styles.searchForm}>
+        <TextInput 
+          style={styles.searchInput}
+          placeholder="Buscar devs por techs..."
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
+        />
+        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+          <MaterialIcons name="my-location" size={20} color="#FFF"/>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
@@ -80,6 +95,42 @@ const styles = StyleSheet.create({
   devTechs: {
     marginTop: 5,
   },
+
+  searchForm: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: 'row',
+  }, 
+
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: '#FFF',
+    color: '#333',
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    elevation: 5,
+  },
+
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#8E4Dff',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 15,
+  }
 })
 
 export default Main;
